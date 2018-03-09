ae349894541ee2b625c37fabfea0157b2128147a
Aersoftler <davidgolla@web.de>
2018-03-09 17:06:36 +0100

    change user-api to more "RESTful"

--- user/user_api.py
+++ user/user_api.py
@@ -6,38 +6,38 @@ from user.User import User
 user_app = Blueprint('user_app', __name__)
 
 
-@user_app.route('/user', methods=['GET'])
-def get_user():
-    user = User(request.args['user']).get_api()
+@user_app.route('/user/<user>', methods=['GET'])
+def get_user(user):
+    user = User(user).get_api()
     return json.dumps(user), 200 if user is not None else json.dumps(user), 404
 
 
-@user_app.route('/user', methods=['PATCH'])
-def patch_user():
+@user_app.route('/user/<user>', methods=['PATCH'])
+def patch_user(user):
     if request.args['update'] == 'password':
         try:
-            User(request.args['user'], password=request.form['new_password'])\
+            User(user, password=request.form['new_password'])\
                 .update_password_api(request.form['old_password'])
         except ValueError as e:
             return e, 400
         return 'Password successfully changed', 200
     elif request.args['update'] == 'display_name':
         try:
-            User(request.args['user'], display_name=request.form['display_name']).update_display_name()
+            User(user, display_name=request.form['display_name']).update_display_name()
         except ValueError as e:
             return e, 400
         return 'display_name successfully changed', 200
     elif request.args['update'] == 'total_score':
-        User(request.args['user']).add_total_score(request.form['score'])
+        User(user).add_total_score(request.form['score'])
         return 'score successfully added to total-score', 200
     else:
         return 'no valid update-parameter', 400
 
 
-@user_app.route('/user', methods=['POST'])
-def post_user():
+@user_app.route('/user/<user>', methods=['POST'])
+def post_user(user):
     try:
-        User(request.args['user'], password=request.form['password']).register()
+        User(user, password=request.form['password']).register()
     except ValueError as e:
         return e, 400
     return 'user successfully registered', 200
