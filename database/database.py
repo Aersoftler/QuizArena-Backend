7cc6ab7b552aba1777f2a7ffdfa95a2f26786bdf
Aersoftler <davidgolla@web.de>
2018-03-22 18:00:55 +0100

    add connection to mLab

--- database/database.py
+++ database/database.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 
-database = MongoClient('localhost', 27017).quizarena
+connection = MongoClient('ds121309.mlab.com', 21309)
+database = connection['quizarena']
+database.authenticate('quizarena', 'c47c2cc8fd')
 user_coll = database.user
 question_coll = database.question
 category_coll = database.category
