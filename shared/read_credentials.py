454b95a66de74a19c430ea719e8a052ebaba8261
Aersoftler <davidgolla@web.de>
2018-04-19 17:37:00 +0200

    fix creds-handling

--- shared/read_credentials.py
+++ shared/read_credentials.py
@@ -1,9 +1,10 @@
 import os
-import sys
 
 import yaml
 
-with open(os.path.join(os.path.dirname(sys.modules['__main__'].__file__), 'credentials.yml'), 'r') as stream:
+import __root__
+
+with open(os.path.join(__root__.path(), 'credentials.yml'), 'r') as stream:
     creds = yaml.load(stream)
     api_key = creds['api']
     mongo_creds = creds['mongo']
