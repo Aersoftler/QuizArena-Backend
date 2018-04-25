89a1e29d9ef34e3da6e9a93e41d1e41f790f2308
Aersoftler <davidgolla@web.de>
2018-04-25 10:04:59 +0200

    adding static page for sharing

--- quizarena.py
+++ quizarena.py
@@ -1,14 +1,13 @@
 from threading import Thread
 
-from flask import Flask
-from flask_sslify import SSLify
+from flask import Flask, send_from_directory
 
 from quiz.Session import Session
 from quiz.category_api import category_app
 from quiz.session_api import session_app
 from user.user_api import user_app
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.register_blueprint(user_app)
 app.register_blueprint(session_app)
 app.register_blueprint(category_app)
@@ -17,8 +16,18 @@ SSLify(app, permanent=True)
 
 
 @app.route('/', methods=['GET'])
-def get():
-    return 'App is running', 200
+def root():
+    return send_from_directory('static', 'QuizArena-lila.png'), 200
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'QuizArena-lila.png'), 200
+
+
+@app.route('/sharing/<session_id>')
+def sharing(session_id):
+    return send_from_directory('static', 'quizarena.html'), 200
 
 
 if __name__ == '__main__':
